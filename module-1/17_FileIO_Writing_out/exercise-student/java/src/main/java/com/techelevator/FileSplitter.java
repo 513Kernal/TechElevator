3c3495de6c36da9a5f61af2de512b6ff030e11de
Kris Hasty <krishasty@gmail.com>
2021-02-02 16:25:43 -0500

    homeworkupdate2.2.21

--- module-1/17_FileIO_Writing_out/exercise-student/java/src/main/java/com/techelevator/FileSplitter.java
+++ module-1/17_FileIO_Writing_out/exercise-student/java/src/main/java/com/techelevator/FileSplitter.java
@@ -1,9 +1,19 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class FileSplitter {
 
 	public static void main(String[] args) {
 
-	}
+		Scanner userInput = new Scanner();
 
+
+
+
+	}
 }
--- module-1/17_FileIO_Writing_out/exercise-student/java/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_FileIO_Writing_out/exercise-student/java/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,53 @@
 package com.techelevator;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.Scanner;
+
 public class FindAndReplace {
 
     public static void main(String[] args) {
 
-    }
+        Scanner userInput = new Scanner(System.in);
 
+        System.out.println("What is the search word?: ");
+        String inputWord = userInput.nextLine();
+
+        System.out.println("What is the replacement word?: ");
+        String replacementWord = userInput.nextLine();
+
+        System.out.println("What is the source file?: ");
+        String sourceFile = userInput.nextLine();
+
+        System.out.println("What is the destination file?: ");
+        String destFile = userInput.nextLine();
+
+        Path source = Path.of(sourceFile);
+        try (Scanner sourceScanner = new Scanner(source)) {
+            try (PrintWriter destinationWriter = new PrintWriter(destFile)) {
+                while (sourceScanner.hasNextLine()){
+                    String line = sourceScanner.nextLine();  //reads the line
+                    if(line.contains(inputWord)){
+                        String replace = line.replaceAll(inputWord, replacementWord);
+                        destinationWriter.println(replace);
+                    } else {
+                        destinationWriter.println(line);
+                    }
+                }
+
+            }catch (FileNotFoundException e) {
+                System.out.println("Output file could not be written to!! ");
+            }
+        } catch (IOException e) {
+            System.out.println("Couldn't read from the input file!!");
+        }
+
+    }
 }
+
+
+
+
+
