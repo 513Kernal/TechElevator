94c0f074a353d4acb9056c2d6e8e5e7e4f56d226
Kris Hasty <krishasty@gmail.com>
2021-02-11 09:28:15 -0500

    homework2.11.21

--- module-2/02_Aggregate_Functions_and_GROUP_BY/lecture-student/mssql/lecture.sql
+++ module-2/02_Aggregate_Functions_and_GROUP_BY/lecture-student/mssql/lecture.sql
@@ -1,48 +1,152 @@
 -- ORDERING RESULTS
 
 -- Populations of all countries in descending order
+SELECT name, population
+FROM country
+ORDER BY population DESC;
+-- best practice to use ASC and DESC
 
 --Names of countries and continents in ascending order
+SELECT name, continent
+FROM country
+ORDER BY name ASC;
 
 -- LIMITING RESULTS
+
 -- The name and average life expectancy of the countries with the 10 highest life expectancies.
+SELECT name, lifeexpectancy
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+ORDER BY lifeexpectancy DESC
+LIMIT 10;
+OFFSET 10; --skips the first 10
+
+--what is the biggest city by population
+SELECT name, population
+FROM city
+WHERE population IS NOT NULL
+ORDER BY population DESC;
 
 -- CONCATENATING OUTPUTS
 
 -- The name & state of all cities in California, Oregon, or Washington.
 -- "city, state", sorted by state then city
+SELECT name ||', '||district AS city_state
+FROM city
+WHERE district IN ('California', 'Oregon', 'Washington')
+ORDER BY district, name;
 
 -- AGGREGATE FUNCTIONS
 -- Average Life Expectancy in the World
+SELECT AVG(lifeexpectancy) AS avg_life_expectancy
+FROM country
+WHERE lifeexpectancy IS NOT NULL;
 
 -- Total population in Ohio
+SELECT SUM(population) AS sum_pop_ohio
+FROM city
+WHERE district = 'Ohio';
 
 -- The surface area of the smallest country in the world
+SELECT surfacearea,name
+FROM country
+ORDER BY surfacearea ASC
+LIMIT 1;
 
 -- The 10 largest countries in the world
+SELECT surfacearea,name
+FROM country
+ORDER BY surfacearea DESC
+LIMIT 10;
+
+SELECT population,name
+FROM country
+ORDER BY population DESC
+LIMIT 10;
 
 -- The number of countries who declared independence in 1991
+SELECT COUNT(*)
+FROM country
+WHERE indepyear = 1991;
 
 -- GROUP BY
+--SELECT,FROM,WHERE(optional) GROUP BY or ORDER BY
 -- Count the number of countries where each language is spoken, ordered from most countries to least
+SELECT language, COUNT(*) AS count_countries
+FROM countrylanguage
+GROUP BY language
+ORDER BY count_countries DESC;
 
 -- Average life expectancy of each continent ordered from highest to lowest
+SELECT continent, AVG(lifeexpectancy)
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+GROUP BY continent
+ORDER BY AVG(lifeexpectancy) DESC;
 
 -- Exclude Antarctica from consideration for average life expectancy
+SELECT continent, AVG(lifeexpectancy)
+FROM country
+WHERE lifeexpectancy IS NOT NULL
+GROUP BY continent
+ORDER BY AVG(lifeexpectancy) DESC;
 
 -- Sum of the population of cities in each state in the USA ordered by state name
+SELECT district, SUM(population) AS sum_pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district
+ORDER BY SUM(population) DESC;
 
--- The average population of cities in each state in the USA ordered by state name
+SELECT district, SUM(population) AS sum_pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district
+ORDER BY district ASC;
+
+-- The average population of cities in each state that has an average population > 1,000,000 in the USA ordered by state name
+SELECT district, round(AVG(population),2) AS avg_pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district
+HAVING AVG(population) > 1000000
+ORDER BY district ASC;
+
+SELECT district, AVG(population) AS avg_pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district
+ORDER BY district DESC;
 
 -- SUBQUERIES
 -- Find the names of cities under a given government leader
+SELECT name, countrycode
+FROM city
+WHERE countrycode IN (
 
+                SELECT code
+                FROM country
+                WHERE headofstate LIKE '%Bush%'
+                );
 -- Find the names of cities whose country they belong to has not declared independence yet
+SELECT name, countrycode
+FROM city
+WHERE countrycode IN(
+
+                SELECT code 
+                FROM country 
+                WHERE indepyear IS NULL);
 
 -- Additional samples
 -- You may alias column and table names to be more descriptive
 
 -- Alias can also be used to create shorthand references, such as "c" for city and "co" for country.
+SELECT name, countrycode AS cc
+FROM city c
+WHERE countrycode IN(
+                SELECT code 
+                FROM country 
+                WHERE indepyear IS NULL);
 
 -- Ordering allows columns to be displayed in ascending order, or descending order (Look at Arlington)
 
@@ -61,7 +165,16 @@
 
 -- Gets the SUM of the population field in the city table, as well as
 -- the AVG population, and a COUNT of the total number of rows.
+SELECT SUM(population) as sum_pop, round(AVG(population),2) as avg_pop, COUNT(*) as total_rows
+FROM city;
 
 -- Gets the MIN population and the MAX population from the city table.
+SELECT district, MIN(population) as min_pop, MAX(population) as max_pop
+FROM city
+WHERE countrycode = 'USA'
+GROUP BY district;
 
 -- Using a GROUP BY with aggregate functions allows us to summarize information for a specific column. For instance, we are able to determine the MIN and MAX population for each countrycode in the city table.
+SELECT countrycode, MIN(population) AS min_pop, MAX(population) AS max_pop
+FROM city
+GROUP BY countrycode;
--- module-2/03_Joins/lecture-student/postgres/lecture.sql
+++ module-2/03_Joins/lecture-student/postgres/lecture.sql
@@ -1,22 +1,68 @@
 -- ********* INNER JOIN ***********
 
 -- Let's find out who made payment 16666:
+SELECT *
+FROM payment
+WHERE payment_id = 16666;
 
 -- Ok, that gives us a customer_id, but not the name. We can use the customer_id to get the name FROM the customer table
+SELECT *
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment_id = 16666;
 
 -- We can see that the * pulls back everything from both tables. We just want everything from payment and then the first and last name of the customer:
+SELECT payment.*, customer.first_name, customer.last_name
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+WHERE payment_id = 16666;
+
 
 -- But when did they return the rental? Where would that data come from? From the rental table, so let’s join that.
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+--INNER JOIN rental ON rental.customer_id = customer.customer_id --all of rosemary rentals, not needed
+INNER JOIN rental ON rental.rental_id = payment.rental_id
+WHERE payment_id = 16666;
 
 -- What did they rent? Film id can be gotten through inventory.
+SELECT payment.*, customer.first_name, customer.last_name, rental.return_date, film.title
+FROM payment
+INNER JOIN customer ON payment.customer_id = customer.customer_id
+INNER JOIN rental ON rental.rental_id = payment.rental_id
+INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+INNER JOIN film ON film.film_id = inventory.film_id
+WHERE payment_id = 16666;
 
 -- What if we wanted to know who acted in that film?
+SELECT film.title, ARRAY_AGG(actor.first_name|| ' ' ||actor.last_name) as actors
+FROM film
+JOIN film_actor ON film_actor.film_id = film.film_id
+JOIN actor ON actor.actor_id = film_actor.actor_id
+WHERE film.film_id = 948
+GROUP BY film.title;
 
 -- What if we wanted a list of all the films and their categories ordered by film title
+SELECT film.title, category.name
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON category.category_id = film_category.category_id
+ORDER BY film.title;
 
 -- Show all the 'Comedy' films ordered by film title
+SELECT film.title, category.name
+FROM film
+JOIN film_category ON film.film_id = film_category.film_id
+JOIN category ON category.category_id = film_category.category_id
+WHERE category.name = 'Comedy'
+ORDER BY film.title;
 
 -- Finally, let's count the number of films under each category
+SELECT film_category.category_id, category.name,COUNT(*)
+FROM film_category
+JOIN category ON category.category_id = film_category.category_id
+GROUP BY film_category.category_id, category.name;
 
 -- ********* LEFT JOIN ***********
 
@@ -24,19 +70,72 @@
 
 -- A Left join, selects all records from the "left" table and matches them with records from the "right" table if a matching record exists.
 
+
+
 -- Let's display a list of all countries and their capitals, if they have some.
+SELECT country.name, city.name
+FROM country
+JOIN city ON country.capital = city.id;
+
 
 -- Only 232 rows
 -- But we’re missing entries:
 
 -- There are 239 countries. So how do we show them all even if they don’t have a capital?
 -- That’s because if the rows don’t exist in both tables, we won’t show any information for it. If we want to show data FROM the left side table everytime, we can use a different join:
-
+SELECT country.name, city.name
+FROM country
+LEFT JOIN city ON country.capital = city.id
+ORDER BY city.name;
 -- *********** UNION *************
 
 -- Back to the "dvdstore" database...
 
 -- Gathers a list of all first names used by actors and customers
 -- By default removes duplicates
+SELECT first_name
+FROM actor
+UNION 
+SELECT first_name
+FROM customer
+
+
 
 -- Gather the list, but this time note the source table with 'A' for actor and 'C' for customer
+SELECT first_name, 'A'
+FROM actor
+UNION 
+SELECT first_name, 'C'
+FROM customer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- What did they rent? Film id can be gotten through inventory.
+WITH find_the_film AS 
+        (SELECT payment.*, customer.first_name, customer.last_name, rental.return_date, film.title
+         FROM payment
+         INNER JOIN customer ON payment.customer_id = customer.customer_id
+         INNER JOIN rental ON rental.rental_id = payment.rental_id
+         INNER JOIN inventory ON inventory.inventory_id = rental.inventory_id
+         INNER JOIN film ON film.film_id = inventory.film_id
+         WHERE payment_id = 16666
+                        )
+
+-- What if we wanted to know who acted in that film?
+SELECT f.title, ARRAY_AGG(a.first_name|| ' ' ||a.last_name) as actors
+FROM film f
+JOIN film_actor fa ON fa.film_id = f.film_id
+JOIN actor a ON a.actor_id = fa.actor_id
+WHERE find_the_film ftf on film_id = 948
+GROUP BY f.title;
