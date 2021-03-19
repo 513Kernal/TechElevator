ec890913d83f90dcbbbd6a2626b9b88b0f14aca1
Kris Hasty <krishasty@gmail.com>
2021-03-19 00:12:00 -0400

    homeworkdom

--- module-3/08_DOM/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/08_DOM/exercise-student/shopping-list/js/shopping-list.js
@@ -1,23 +1,84 @@
 // add pageTitle
+const pageTitle = 'My Shopping List';
 
 // add groceries
+const groceries = [{
+  item: 'pepperoni'
+},
+{
+  item: 'dough'
+},
+{
+  item: 'cheese'
+},
+{
+  item: 'sauce'
+},
+{
+  item: 'oregano'
+},
+{
+  item: 'bacon'
+},
+{
+  item: 'sausage'
+},
+{
+  item: 'pineapple'
+},
+{
+  item: 'salami'
+},
+{
+  item: 'ham'
+},
+]
 
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function setPageTitle() {}
+function setPageTitle() {
+   const title = document.getElementById('title');
+  title.innerText = pageTitle;
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+const storeStuff = document.getElementById('groceries')
+groceries.forEach( food => {
+const container = document.createElement('groceries')
+  container.setAttribute('id','groceries')
+  addItem(container,food.item)
 
+  storeStuff.insertAdjacentElement('beforeend', container)
+
+} )
+
+
+
+}
+function addItem(parent, item){
+  const itemList = document.createElement('li')
+  const listItem =itemList.innerText = item
+  parent.appendChild(itemList)
+}
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
-function markCompleted() {}
+function markCompleted() {
+  const items = document.querySelectorAll('li');
+  items.forEach((item) => {
+    item.setAttribute('class', 'completed');
+  } );
+  
+  
+
+  
+}
 
 setPageTitle();
 
