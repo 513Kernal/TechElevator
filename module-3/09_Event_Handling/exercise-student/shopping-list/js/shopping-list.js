fc1b135c148c18a7719a29dde1b497e8c43e24cb
Kris Hasty <krishasty@gmail.com>
2021-03-22 00:04:45 -0400

    homeworknotalltestsrun

--- module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
+++ module-3/09_Event_Handling/exercise-student/shopping-list/js/shopping-list.js
@@ -39,3 +39,52 @@ function displayGroceries() {
 
 setPageTitle();
 displayGroceries();
+
+
+function markCompleted() {
+  const list = document.querySelector('ul')
+  const items = Array.from(list.children)
+  
+
+  items.forEach((item) => {
+    if (allItemsComplete == false) {
+      item.setAttribute('class', 'completed')
+      item.querySelector('i').classList.add('completed')
+    } else if (allItemsComplete == true) {
+      item.setAttribute('class', '') 
+      item.querySelector('i').classList.remove('completed')
+    }
+  });
+}
+
+const button = document.querySelector('.btn');
+button.addEventListener('click', markAllComplete);
+
+const li = document.querySelector('li');
+li.addEventListener('click', markAllComplete)
+
+function markAllComplete() {
+  const a = document.querySelector('a')
+  markCompleted();
+  if (allItemsComplete == true) {
+    allItemsComplete = false
+    a.innerText = 'Mark All Complete'
+  } else {
+    allItemsComplete = true
+    a.innerText = 'Mark All Incomplete'
+  }
+}
+
+
+const list = document.querySelector('ul')
+
+list.addEventListener('click', (event) => {
+  if(!event.target.classList.add('completed')) {
+    event.target.classList.add('completed');
+    event.target.querySelector('i').classList.add('completed')
+  } else if (event.target.classList == 'completed') {
+    event.target.classList.remove('completed')
+    event.target.querySelector('i').classList.remove('completed')
+  }
+
+})
