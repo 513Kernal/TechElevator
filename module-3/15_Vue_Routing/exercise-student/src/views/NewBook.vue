19a4c54200489f13efd99d4e855fe006dcd88339
Kris Hasty <krishasty@gmail.com>
2021-04-01 00:16:22 -0400

    notalltestspass

---
+++ module-3/15_Vue_Routing/exercise-student/src/views/NewBook.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <new-book-form />
+  </div>
+</template>
+
+<script> // JavaScript
+import NewBookForm from '../components/NewBookForm.vue';
+  export default { 
+    components: { 
+        NewBookForm
+    }
+  }
+</script>
+<style scoped>
